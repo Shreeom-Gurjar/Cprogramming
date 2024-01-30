751bb14d2b2fcbb21be41f15eb763bc379ce8ead
Shreeom-Gurjar <141802307+Shreeom-Gurjar@users.noreply.github.com>
2024-01-30 11:33:23 +0530

    Cprogramming

---
+++ abc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {    
+
+    int number1=10, number2=20, sum;
+    
+    printf("Enter two integers: ");
+    sum = number1 * number2;      
+    
+    printf("%d *%d = %d", number1, number2, sum);
+    
+}
