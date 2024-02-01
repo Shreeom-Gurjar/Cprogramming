bcb20b3e9155ad977c11d4bcd790acc509a300b6
Shreeom-Gurjar <141802307+Shreeom-Gurjar@users.noreply.github.com>
2024-02-01 14:31:08 +0530

    c program

---
+++ operation2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    printf("Enter a number"); 
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf("%d\n",c);
+    printf("%d\n",d);
+    printf("%d\n",e);
+    printf("%d",f);
+}
